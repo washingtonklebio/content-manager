43a6de4c1d307ccebb08d3f39836bd09ad8f663a
Washington <washington@localhost.localdomain>
2019-08-28 15:47:30 -0300

    Creating article actions

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -26,4 +26,18 @@ module.exports = app => {
         .get(app.api.category.getById)
         .put(app.api.category.save)
         .delete(app.api.category.remove)
+
+    app.route('/articles')
+        .all(app.config.passport.authenticate())
+        .get(app.api.article.get)
+        .post(app.api.article.save)
+
+    app.route('/articles/:id')
+        .all(app.config.passport.authenticate())
+        .get(app.api.article.getById)
+        .put(app.api.article.save)
+        .delete(app.api.article.remove)
+
+    app.route('/categories/:id/articles')
+        .get(app.api.article.getByCategory)
 }
