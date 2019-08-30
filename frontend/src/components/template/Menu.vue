66c53408bab4798c3b9d39e8e1b8826cebdd0b0e
Washington <washington@localhost.localdomain>
2019-08-30 12:40:20 -0300

    Create components template

---
+++ frontend/src/components/template/Menu.vue
@@ -0,0 +1,20 @@
+<template>
+	<aside class="menu"></aside>
+</template>
+
+<script>
+export default {
+	name: "Menu",
+}
+</script>
+
+<style>
+    .menu {
+        grid-area: menu;
+        background: linear-gradient(to right, #232426, #414345);
+        
+        display: flex;
+        flex-direction: flex;
+        flex-wrap: wrap;
+    }
+</style>
